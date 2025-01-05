0110affad7067d41bec6c6dc9e9e13ead33f5e80
WhatDidYouExpect <89535984+WhatDidYouExpect@users.noreply.github.com>
2025-01-05 20:36:18 +0100

    Update whoami.py

--- whoami.py
+++ whoami.py
@@ -2,17 +2,23 @@ import discord
 from discord.ext import commands
 
 class whoami(commands.Cog):
-  def __init__(self, bot):
-       self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-  @commands.command()
-  async def whoami(self, ctx):
-       user_id = ctx.author.id
-       username = ctx.author.name
-       await ctx.send(
-           f"Your User ID is: {user_id}\n"
-           f"Your username is: {username}\n"
-           f"Your nickname in this server is: <@{user_id}>"
-           )
+    @commands.command()
+    async def whoami(self, ctx):
+        user_id = ctx.author.id
+        username = ctx.author.name
+
+        embed = discord.Embed(
+            title="User Information",
+            description=f"Your User ID is: {user_id}\n"
+                        f"Your username is: {username}\n"
+                        f"Your nickname in this server is: <@{user_id}>",
+            color=discord.Color.blue() 
+        )
+
+        await ctx.send(embed=embed)
+
 async def setup(bot):
-   await bot.add_cog(whoami(bot))
+    await bot.add_cog(whoami(bot))
