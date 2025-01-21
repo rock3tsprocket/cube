c87d97da280be1953d276922d684b84ded61a1ae
WhatDidYouExpect <89535984+WhatDidYouExpect@users.noreply.github.com>
2025-01-21 17:05:45 +0100

    Update cogmanager.py

--- customcommands/cogmanager.py
+++ customcommands/cogmanager.py
@@ -1,14 +1,15 @@
 import discord
 from discord.ext import commands
 import os 
+from config import ownerid
 
 class CogManager(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.owner_id = int(os.getenv("ownerid"))
+
     @commands.command()
     async def load(self, ctx, cog_name: str = None):
-        if ctx.author.id != self.owner_id:
+        if ctx.author.id != ownerid:
             await ctx.send("You do not have permission to use this command.")
             return
         if cog_name is None:
@@ -22,7 +23,7 @@ class CogManager(commands.Cog):
 
     @commands.command()
     async def unload(self, ctx, cog_name: str = None):
-        if ctx.author.id != self.owner_id:
+        if ctx.author.id != ownerid:
             await ctx.send("You do not have permission to use this command.")
             return
         if cog_name is None:
@@ -36,8 +37,7 @@ class CogManager(commands.Cog):
 
     @commands.command()
     async def reload(self, ctx, cog_name: str = None):
-        
-        if ctx.author.id != self.owner_id:
+        if ctx.author.id != ownerid:
             await ctx.send("You do not have permission to use this command.")
             return
         if cog_name is None:
@@ -49,6 +49,18 @@ class CogManager(commands.Cog):
             await ctx.send(f"Reloaded cog `{cog_name}` successfully.")
         except Exception as e:
             await ctx.send(f"Error reloading cog `{cog_name}`: {e}")
+
+    @commands.command()
+    async def listcogs(self, ctx):
+        """Lists all currently loaded cogs in an embed."""
+        cogs = list(self.bot.cogs.keys())
+        if not cogs:
+            await ctx.send("No cogs are currently loaded.")
+            return
+
+        embed = discord.Embed(title="Loaded Cogs", description="Here is a list of all currently loaded cogs:")
+        embed.add_field(name="Cogs", value="\n".join(cogs), inline=False)
+        await ctx.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(CogManager(bot))
